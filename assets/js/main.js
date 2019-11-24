21337c5f3f7910eaa5a30ac55a9f89fea7fea0a1
Ross Markello <rossmarkello@gmail.com>
2019-11-24 11:16:04 -0500

    Better mobile menu

---
+++ assets/js/main.js
@@ -0,0 +1,104 @@
+/**
+ * Utils
+ */
+
+// Throttle
+//
+const throttle = (callback, limit) => {
+  let timeoutHandler = null;
+  return () => {
+    if (timeoutHandler == null) {
+      timeoutHandler = setTimeout(() => {
+        callback();
+        timeoutHandler = null;
+      }, limit);
+    }
+  };
+};
+
+// addEventListener Helper
+//
+const listen = (ele, e, callback) => {
+  if (document.querySelector(ele) !== null) {
+    document.querySelector(ele).addEventListener(e, callback);
+  }
+}
+
+/**
+ * Functions
+ */
+
+// Auto Hide Header
+//
+let header = document.getElementById('site-header');
+let lastScrollPosition = window.pageYOffset;
+
+const autoHideHeader = () => {
+  let currentScrollPosition = Math.max(window.pageYOffset, 0);
+  if (currentScrollPosition > lastScrollPosition) {
+    header.classList.remove('slideInUp');
+    header.classList.add('slideOutDown');
+  } else {
+    header.classList.remove('slideOutDown');
+    header.classList.add('slideInUp');
+  }
+  lastScrollPosition = currentScrollPosition;
+}
+
+// Mobile Menu Toggle
+//
+let mobileMenuVisible = false;
+
+const toggleMobileMenu = () => {
+  let mobileMenu = document.getElementById('mobile-menu');
+  let bottom = document.getElementById('site-header').clientHeight + 'px';
+  if (mobileMenuVisible == false) {
+    mobileMenu.style.animationName = 'bounceInRight';
+    mobileMenu.style.webkitAnimationName = 'bounceInRight';
+    mobileMenu.style.display = 'block';
+    mobileMenu.style.bottom = bottom;
+    mobileMenuVisible = true;
+  } else {
+    mobileMenu.style.animationName = 'bounceOutRight';
+    mobileMenu.style.webkitAnimationName = 'bounceOutRight'
+    mobileMenuVisible = false;
+  }
+}
+
+// Featured Image Toggle
+//
+const showImg = () => {
+  document.querySelector('.bg-img').classList.add('show-bg-img');
+}
+
+const hideImg = () => {
+  document.querySelector('.bg-img').classList.remove('show-bg-img');
+}
+
+// ToC Toggle
+//
+const toggleToc = () => {
+  document.getElementById('toc').classList.toggle('show-toc');
+}
+
+
+if (header !== null) {
+  listen('#menu-btn', "click", toggleMobileMenu);
+  listen('#toc-btn', "click", toggleToc);
+  listen('#img-btn', "click", showImg);
+  listen('.bg-img', "click", hideImg);
+
+  document.querySelectorAll('.post-year').forEach((ele)=> {
+    ele.addEventListener('click', () => {
+      window.location.hash = '#' + ele.id;
+    });
+  });
+
+  window.addEventListener('scroll', throttle(() => {
+    // autoHideHeader();
+
+    if (mobileMenuVisible == true) {
+      toggleMobileMenu();
+    }
+  }, 250));
+}
